d0fd803a026966dc6328c2aac2769eaecda3aad0
Angel González <anngeeldavid@gmail.com>
2022-09-22 19:57:49 -0600

    Agregar archivos de proyecto.

---
+++ restaconstructor/resta.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace restaconstructor
+{
+    class resta
+    {
+        public double V1;
+        public double V2;
+
+        public resta()
+        {
+        }
+
+        public resta(double v1, double v2)
+        {
+            V1 = v1;
+            V2 = v2;
+        }
+
+        public double restar()
+        {
+            return V1 - V2;
+        }
+    }
+    
+}
